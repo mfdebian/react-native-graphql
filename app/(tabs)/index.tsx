59d201947065873289586bfa5c99c0281f728b0a
Alfredo González <alfredo.arosteguy@gmail.com>
2024-08-22 22:24:34 -0400

    removes boilerplate unused components, set app title

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,16 @@
+import { useEffect } from 'react';
 import { Launches } from '@/components/Launches';
-import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: "https://spacex-production.up.railway.app",
-  cache: new InMemoryCache()
+  uri: 'https://spacex-production.up.railway.app',
+  cache: new InMemoryCache(),
 });
 
 export default function HomeScreen() {
+  useEffect(() => {
+    document.title = 'React Native GraphQL';
+  }, []);
   return (
     <ApolloProvider client={client}>
       <Launches />
